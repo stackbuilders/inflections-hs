cfffa419291cf53efab211082fa3936d8c23b154
Justin Leitgeb <justin@stackbuilders.com>
2014-03-08 09:54:41 -0500

    Remove some qualifications

--- Text/Inflections/Parse/SnakeCase.hs
+++ Text/Inflections/Parse/SnakeCase.hs
@@ -3,18 +3,17 @@
 module Text.Inflections.Parse.SnakeCase ( Word(..), parser )
 where
 
-import qualified Text.ParserCombinators.Parsec.Char as C
-import qualified Text.Parsec as P
 import Control.Applicative ((<$>))
+import Text.Parsec
 
 import Text.Inflections.Parse.Types (Word(..))
 import Text.Inflections.Parse.Acronym (acronym)
 
-word :: P.Stream s m Char => P.ParsecT s u m Word
-word = Word <$> ((P.many1 C.lower) P.<|> (P.many1 C.digit))
+word :: Stream s m Char => ParsecT s u m Word
+word = Word <$> ((many1 lower) <|> (many1 digit))
 
-parser :: P.Stream s m Char => [String] -> P.ParsecT s u m [Word]
+parser :: Stream s m Char => [String] -> ParsecT s u m [Word]
 parser acronyms = do
-  ws <- (acronym acronyms P.<|> word) `P.sepBy` (C.char '_')
-  P.eof
+  ws <- (acronym acronyms <|> word) `sepBy` (char '_')
+  eof
   return ws
