bf6443097009da12bea558a9f697173dc888463b
brady.ouren <brady.ouren@gmail.com>
2016-10-02 14:57:27 -0700

    remove tests for non-exported functions

--- test/Text/Inflections/CountableSpec.hs
+++ test/Text/Inflections/CountableSpec.hs
@@ -3,19 +3,14 @@
 module Text.Inflections.CountableSpec (spec) where
 
 import Test.Hspec
-import Data.Maybe
 
 import Text.Inflections
-import Text.Inflections.Countable
 
 spec :: Spec
 spec = do
   irregularCases
   matchingCases
 
-reg (p, r) = matchWithReplace (pat, r)
-  where
-    pat = fromJust $ regexPattern p
 matchingCases :: Spec
 matchingCases = do
   describe "laws" $ do
@@ -27,23 +22,6 @@ matchingCases = do
 
     it "returns itself when applied to (pluralize . singularize)" $
       mapM_ equality2 ["things", "branches", "oxen"]
-
-  describe "matchWithReplace" $ do
-    it "replaces the non-grouped patterns" $ do
-      let in_n_out = [ (("s$", ""), ("things", "thing"))
-                     , (("$", "s"), ("thing", "things"))
-                     ]
-
-      mapM_ (\(in', out) -> fromJust(reg in' (fst out)) `shouldBe` snd out ) in_n_out
-
-    it "replaces the grouped patterns for singularize" $ do
-      let in_n_out = [ (("(ch)es$", "\1"), ("branches", "branch"))
-                     , (("^(ox)en", "\1"), ("oxen", "ox"))
-                     , (("(octop|vir)us$", "\1i"), ("octopus", "octopi"))
-                     , (("((a)naly|(b)a|(d)iagno|(p)arenthe|(p)rogno|(s)ynop|(t)he)(sis|ses)$", "\1sis"), ("analyses", "analysis"))
-                     ]
-
-      mapM_ (\(in', out) -> fromJust(reg in' (fst out)) `shouldBe` snd out ) in_n_out
 
   describe "pluralize" $ do
     it "handles the normal case" $ do
