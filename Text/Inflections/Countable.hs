9db8f28b63f053576985d01262fe3a35cc902d5d
brady.ouren <brady.ouren@gmail.com>
2016-10-02 11:06:22 -0700

    get simple matching cases working

--- Text/Inflections/Countable.hs
+++ Text/Inflections/Countable.hs
@@ -1,57 +1,96 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Text.Inflections.Countable where
 
-import Data.Maybe (fromMaybe, catMaybes)
-import Data.Text
+import Data.Maybe (fromJust, fromMaybe, catMaybes)
+import Data.Text as T
+import Data.Text.Encoding (encodeUtf8, decodeUtf8)
 import Text.Inflections.Data
-import Text.Regex.TDFA.ByteString as Regex
+import Text.Regex.PCRE.Light
+import Data.Monoid ((<>))
+import Data.ByteString.Char8
 
-type RegexPattern = Text
-type RegexReplace = Text
-type Singular = Text
-type Plural = Text
+type RegexPattern = T.Text
+type RegexReplace = T.Text
+type Singular = T.Text
+type Plural = T.Text
 
 data Inflection
   = Simple (Singular, Plural)
   | Match (RegexPattern, RegexReplace)
 
-pluralize :: Text -> Text
+pluralize :: T.Text -> T.Text
 pluralize = pluralizeWith mapping
   where
     mapping = defaultIrregulars ++ defaultUncountables ++ defaultPlurals
 
-singularize :: Text -> Text
+singularize :: T.Text -> T.Text
 singularize = singularizeWith mapping
   where
     mapping = defaultIrregulars ++ defaultUncountables ++ defaultSingulars
 
-pluralizeWith :: [Inflection] -> Text -> Text
+pluralizeWith :: [Inflection] -> T.Text -> T.Text
 pluralizeWith mapping t = fromMaybe t $ headMaybe matches
   where
-    matches = catMaybes $ fmap (pluralLookup t) mapping 
+    matches = catMaybes $ fmap (pluralLookup t) (Prelude.reverse mapping)
 
-singularizeWith :: [Inflection] -> Text -> Text
+singularizeWith :: [Inflection] -> T.Text -> T.Text
 singularizeWith mapping t = fromMaybe t $ headMaybe matches
   where
-    matches = catMaybes $ fmap (singularLookup t) mapping
+    matches = catMaybes $ fmap (singularLookup t) (Prelude.reverse mapping)
 
-pluralLookup :: Text -> Inflection -> Maybe Text
-pluralLookup t (Match (r1,r2)) = Nothing
+pluralLookup :: T.Text -> Inflection -> Maybe T.Text
+pluralLookup t (Match (r1,r2)) = runSub (r1,r2) t
 pluralLookup t (Simple (a,b)) = if t == a then (Just b) else Nothing
 
-singularLookup :: Text -> Inflection -> Maybe Text
-singularLookup t (Match (r1,r2)) = Nothing
+singularLookup :: T.Text -> Inflection -> Maybe T.Text
+singularLookup t (Match (r1,r2)) = runSub (r1,r2) t
 singularLookup t (Simple (a,b)) = if t == b then (Just a) else Nothing
+
+runSub :: (RegexPattern, RegexReplace) -> T.Text -> Maybe T.Text
+runSub (pat, rep) t = case (regexPattern pat, rep) of
+  (Nothing, _) -> Nothing
+  (Just reg, rep) -> matchWithReplace (reg, rep) t
+
+matchWithReplace :: (Regex, RegexReplace) -> T.Text -> Maybe T.Text
+matchWithReplace (reg, rep) t = case regexMatch t reg of
+  Nothing -> Nothing
+  Just (matched : grouping) ->
+    pure $ (t `without` matched) <> groupReplace grouping
+  where
+    groupReplace g = fromMaybe "" $ headMaybe g <> addition
+    addition = headMaybe $ fromMaybe [] $ tailMaybe $ T.split ((==) '\1') rep
+
+without :: T.Text -> T.Text -> T.Text
+without t1 t2 = if t2 == "" then t1 else T.replace t2 "" t1
+
+regexMatch :: T.Text -> Regex -> Maybe [T.Text]
+regexMatch t r = case match r t' [] of
+  Just bs -> Just $ fmap decodeUtf8 bs
+  Nothing -> Nothing
+  where
+    t' = encodeUtf8 t
+
+regexPattern :: T.Text -> Maybe Regex
+regexPattern pat = eitherToMaybe $ compileM (encodeUtf8 pat) [caseless]
+
+eitherToMaybe e = case e of
+  Left _ -> Nothing
+  Right r -> Just r
 
 headMaybe [] = Nothing
 headMaybe (x:_) = Just x
 
-makeMatchMapping :: [(Text, Text)] -> [Inflection]
-makeMatchMapping = fmap Match
+tailMaybe [] = Nothing
+tailMaybe (_:xs) = Just xs
 
-makeIrregularMapping :: [(Text, Text)] -> [Inflection]
+makeMatchMapping :: [(T.Text, T.Text)] -> [Inflection]
+makeMatchMapping = fmap Match
+  --TODO compile the regex
+
+makeIrregularMapping :: [(T.Text, T.Text)] -> [Inflection]
 makeIrregularMapping = fmap Simple
 
-makeUncountableMapping :: [Text] -> [Inflection]
+makeUncountableMapping :: [T.Text] -> [Inflection]
 makeUncountableMapping = fmap (\a -> (Simple (a,a)))
 
 defaultPlurals :: [Inflection]
@@ -65,21 +104,3 @@ defaultIrregulars = makeIrregularMapping defaultIrregulars'
 
 defaultUncountables :: [Inflection]
 defaultUncountables = makeUncountableMapping defaultUncountables'
-
--- regexPattern :: Text -> Either String Regex.Regex
--- regexPattern bs = Regex.compile comp ex bs
---   where
---     comp = defaultCompOpt { caseSensitive = False }
---     ex = ExecOption { captureGroups = True }
-
--- regexMatch :: Text -> Bool
--- regexMatch bs =
---     case regexMatch' txt of
---       Left _ -> False
---       Right mmatch -> case mmatch of
---                         Just a -> True
---                         Nothing -> False
-
--- regexMatch' p =
---       Regex.execute (regexPattern p)
---     . ByteString.Lazy.toStrict
--- test/Text/Inflections/CountableSpec.hs
+++ test/Text/Inflections/CountableSpec.hs
@@ -3,8 +3,10 @@
 module Text.Inflections.CountableSpec (spec) where
 
 import Test.Hspec
+import Data.Maybe
 
 import Text.Inflections
+import Text.Inflections.Countable
 
 spec :: Spec
 spec = do
@@ -13,11 +15,32 @@ spec = do
 
 matchingCases :: Spec
 matchingCases = do
+  it "replaces the non-grouped patterns" $ do
+    let reg = fromJust $ regexPattern "s$"
+    fromJust (matchWithReplace (reg, "") "things") `shouldBe` "thing"
+
+  it "replaces the grouped patterns" $ do
+    let reg = fromJust $ regexPattern "(ch)es$"
+    fromJust (matchWithReplace (reg, "\1") "branches") `shouldBe` "branch"
+
+    let reg2 = fromJust $ regexPattern "^(ox)$"
+    fromJust (matchWithReplace (reg2, "\1en") "ox") `shouldBe` "oxen"
+
+  it "handles the normal case" $ do
+    pluralize "thing" `shouldBe` "things"
+
   it "pluralizes regex cases" $ do
     pluralize "ox" `shouldBe` "oxen"
 
   it "singularizes regex cases" $ do
     singularize "mice" `shouldBe` "mouse"
+    singularize "oxen" `shouldBe` "ox"
+    singularize "branches" `shouldBe` "branch"
+
+  -- TODO stop lazy eval?
+  it "returns itself when applied to both pluralize and singularize" $
+    mapM_ equality ["thing", "branch", "ox"]
+      where equality a = a `shouldBe` (singularize . pluralize) a
 
 irregularCases :: Spec
 irregularCases = do
@@ -36,7 +59,3 @@ irregularCases = do
 
   it "can singularize irregulars" $
     singularize "people" `shouldBe` "person"
-
-  -- it "returns itself when applied to both pluralize and singularize" $
-  --   mapM_ equality (map fst defaultIrregulars)
-  --     where equality a = a `shouldBe` (singularize . pluralize) a
