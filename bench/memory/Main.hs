1b1bddfaeaa1ffcbcc52fe52171502e4efd54e85
CristhianMotoche <cristhian.motoche@gmail.com>
2023-09-17 23:09:02 -0500

    ref: Use all text to benchmark

--- bench/memory/Main.hs
+++ bench/memory/Main.hs
@@ -1,21 +1,44 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
-import Weigh
+import           Weigh
 
-import Data.Text (Text, pack)
+import           Prelude          hiding (readFile, take)
 
-import Text.Inflections
-  ( toUnderscore
-  )
+import           Data.Text        (Text, pack, take)
+import           Data.Text.IO     (readFile)
+
+
+import           Text.Inflections (toCamelCased, toDashed, toHumanized,
+                                   toUnderscore)
 
 
 main :: IO ()
-main = mainWith $ do
-  let textLen :: Int -> Text
-      textLen n = pack (take n ['0'..])
-  func "toUnderscore empty" toUnderscore mempty
-  func "toUnderscore text-len-10" toUnderscore (textLen 10)
-  func "toUnderscore text-len-100" toUnderscore (textLen 100)
-  func "toUnderscore text-len-1000" toUnderscore (textLen 1000)
+main = do
+  text <- readFile "bench/little_women.txt"
+  mainWith $ do
+    let !t1 = take 100 text
+        !t2 = take 10000 text
+        !t3 = take 1000000 text
+
+    func "toUnderscore text-len-100" toUnderscore t1
+    func "toUnderscore text-len-10000" toUnderscore t2
+    func "toUnderscore text-len-1000000" toUnderscore t3
+    func "toUnderscore all text" toUnderscore text
+
+    func "toDashed text-len-100" toDashed t1
+    func "toDashed text-len-10000" toDashed t2
+    func "toDashed text-len-1000000" toDashed t3
+    func "toDashed all text" toDashed text
+
+    func "toCamelCased False text-len-100" (toCamelCased False) t1
+    func "toCamelCased False text-len-10000" (toCamelCased False) t2
+    func "toCamelCased False text-len-1000000" (toCamelCased False) t3
+    func "toCamelCased False all text" (toCamelCased False) text
+
+    func "toHumanized False text-len-100" (toHumanized False) t1
+    func "toHumanized False text-len-10000" (toHumanized False) t2
+    func "toHumanized False text-len-1000000" (toHumanized False) t3
+    func "toHumanized False all text" (toHumanized False) text
--- bench/speed/Main.hs
+++ bench/speed/Main.hs
@@ -10,7 +10,8 @@ import           Prelude          hiding (readFile, take)
 import           Data.Text        (Text, pack, take)
 import           Data.Text.IO     (readFile)
 
-import           Text.Inflections (toUnderscore)
+import           Text.Inflections (toCamelCased, toDashed, toHumanized,
+                                   toUnderscore)
 
 
 main :: IO ()
@@ -24,5 +25,24 @@ main = do
       [ bench "text-len-100" $ nf toUnderscore t1
       , bench "text-len-10000" $ nf toUnderscore t2
       , bench "text-len-1000000" $ nf toUnderscore t3
+      , bench "all text" $ nf toUnderscore text
+      ]
+    , bgroup "toDashed"
+      [ bench "text-len-100" $ nf toDashed t1
+      , bench "text-len-10000" $ nf toDashed t2
+      , bench "text-len-1000000" $ nf toDashed t3
+      , bench "all text" $ nf toDashed text
+      ]
+    , bgroup "toCamelCased False"
+      [ bench "text-len-100" $ nf (toCamelCased False) t1
+      , bench "text-len-10000" $ nf (toCamelCased False) t2
+      , bench "text-len-1000000" $ nf (toCamelCased False) t3
+      , bench "all text" $ nf (toCamelCased False) text
+      ]
+    , bgroup "toHumanized False"
+      [ bench "text-len-100" $ nf (toHumanized False) t1
+      , bench "text-len-10000" $ nf (toHumanized False) t2
+      , bench "text-len-1000000" $ nf (toHumanized False) t3
+      , bench "all text" $ nf (toHumanized False) text
       ]
     ]
