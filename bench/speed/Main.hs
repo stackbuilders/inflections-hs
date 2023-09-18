11eed9fd60c53f6f42c32b3eb46ec163f375288c
CristhianMotoche <cristhian.motoche@gmail.com>
2023-09-17 22:52:50 -0500

    ref: Use text from a book

--- bench/speed/Main.hs
+++ bench/speed/Main.hs
@@ -1,25 +1,28 @@
+{-# LANGUAGE BangPatterns      #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
-import Criterion.Main
+import           Criterion.Main
 
-import Data.Text (Text, pack)
+import           Prelude          hiding (readFile, take)
 
-import Text.Inflections
-  ( toUnderscore
-  )
+import           Data.Text        (Text, pack, take)
+import           Data.Text.IO     (readFile)
+
+import           Text.Inflections (toUnderscore)
 
 
 main :: IO ()
-main =
+main = do
+  text <- readFile "bench/little_women.txt"
+  let !t1 = take 100 text
+      !t2 = take 10000 text
+      !t3 = take 1000000 text
   defaultMain
     [ bgroup "toUnderscore"
-      [ bench "mempty" $ nf toUnderscore mempty
-      , bench "text-len-10" $ nf toUnderscore (textLen 10)
-      , bench "text-len-100" $ nf toUnderscore (textLen 100)
-      , bench "text-len-1000" $ nf toUnderscore (textLen 1000)
+      [ bench "text-len-100" $ nf toUnderscore t1
+      , bench "text-len-10000" $ nf toUnderscore t2
+      , bench "text-len-1000000" $ nf toUnderscore t3
       ]
     ]
-  where textLen :: Int -> Text
-        textLen n = pack (take n ['0'..])
