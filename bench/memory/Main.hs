74fb91f0f4ab7614541d7c618f02837f29e73a48
CristhianMotoche <cristhian.motoche@gmail.com>
2023-09-19 20:47:53 -0500

    feat: Include more benchmarks

--- bench/memory/Main.hs
+++ bench/memory/Main.hs
@@ -5,14 +5,16 @@ module Main (main) where
 
 import           Weigh
 
-import           Prelude          hiding (readFile, take)
+import           Prelude          hiding (readFile, take, words)
 
-import           Data.Text        (Text, pack, take)
+import           Data.Maybe (catMaybes)
+
+import           Data.Text        (Text, pack, take, words)
 import           Data.Text.IO     (readFile)
 
 
-import           Text.Inflections (toCamelCased, toDashed, toHumanized,
-                                   toUnderscore)
+import           Text.Inflections (SomeWord(..), mkWord, mkAcronym, ordinal, parameterize,
+                                   titleize, toCamelCased, toDashed, toHumanized, toUnderscore)
 
 
 main :: IO ()
@@ -22,6 +24,12 @@ main = do
     let !t1 = take 100 text
         !t2 = take 10000 text
         !t3 = take 1000000 text
+
+        somewords txt = catMaybes $ mkSomeWords $ words txt
+
+        mkSomeWords (f:s:rest) = (SomeWord <$> mkWord f) : (SomeWord <$> mkAcronym s) : mkSomeWords rest
+        mkSomeWords (f:rest) = (SomeWord <$> mkWord f) : mkSomeWords rest
+        mkSomeWords [] = []
 
     func "toUnderscore text-len-100" toUnderscore t1
     func "toUnderscore text-len-10000" toUnderscore t2
@@ -42,3 +50,18 @@ main = do
     func "toHumanized False text-len-10000" (toHumanized False) t2
     func "toHumanized False text-len-1000000" (toHumanized False) t3
     func "toHumanized False all text" (toHumanized False) text
+
+    func "parameterize text-len-100" parameterize t1
+    func "parameterize text-len-10000" parameterize t2
+    func "parameterize text-len-1000000" parameterize t3
+    func "parameterize all text" parameterize text
+
+    func "titleize text-len-100" titleize (somewords t1)
+    func "titleize text-len-10000" titleize (somewords t2)
+    func "titleize text-len-1000000" titleize (somewords t3)
+    func "titleize all text" titleize (somewords text)
+      
+    func "ordinal 1" ordinal 1
+    func "ordinal 3" ordinal 13
+    func "ordinal 13" ordinal 13
+    func "ordinal 137" ordinal 137
--- bench/speed/Main.hs
+++ bench/speed/Main.hs
@@ -5,13 +5,15 @@ module Main (main) where
 
 import           Criterion.Main
 
-import           Prelude          hiding (readFile, take)
+import           Prelude          hiding (readFile, take, words)
 
-import           Data.Text        (Text, pack, take)
+import           Data.Maybe (catMaybes)
+
+import           Data.Text        (Text, pack, take, words)
 import           Data.Text.IO     (readFile)
 
-import           Text.Inflections (toCamelCased, toDashed, toHumanized,
-                                   toUnderscore)
+import           Text.Inflections (SomeWord(..), mkWord, mkAcronym, ordinal, parameterize,
+                                   titleize, toCamelCased, toDashed, toHumanized, toUnderscore)
 
 
 main :: IO ()
@@ -20,29 +22,37 @@ main = do
   let !t1 = take 100 text
       !t2 = take 10000 text
       !t3 = take 1000000 text
+
+      somewords txt = catMaybes $ mkSomeWords $ words txt
+
+      mkSomeWords (f:s:rest) = (SomeWord <$> mkWord f) : (SomeWord <$> mkAcronym s) : mkSomeWords rest
+      mkSomeWords (f:rest) = (SomeWord <$> mkWord f) : mkSomeWords rest
+      mkSomeWords [] = []
+
+      benchTextToText name func =
+        bgroup name
+          [ bench "text-len-100" $ nf func t1
+          , bench "text-len-10000" $ nf func t2
+          , bench "text-len-1000000" $ nf func t3
+          , bench "all text" $ nf func text
+          ]
+
   defaultMain
-    [ bgroup "toUnderscore"
-      [ bench "text-len-100" $ nf toUnderscore t1
-      , bench "text-len-10000" $ nf toUnderscore t2
-      , bench "text-len-1000000" $ nf toUnderscore t3
-      , bench "all text" $ nf toUnderscore text
-      ]
-    , bgroup "toDashed"
-      [ bench "text-len-100" $ nf toDashed t1
-      , bench "text-len-10000" $ nf toDashed t2
-      , bench "text-len-1000000" $ nf toDashed t3
-      , bench "all text" $ nf toDashed text
-      ]
-    , bgroup "toCamelCased False"
-      [ bench "text-len-100" $ nf (toCamelCased False) t1
-      , bench "text-len-10000" $ nf (toCamelCased False) t2
-      , bench "text-len-1000000" $ nf (toCamelCased False) t3
-      , bench "all text" $ nf (toCamelCased False) text
-      ]
-    , bgroup "toHumanized False"
-      [ bench "text-len-100" $ nf (toHumanized False) t1
-      , bench "text-len-10000" $ nf (toHumanized False) t2
-      , bench "text-len-1000000" $ nf (toHumanized False) t3
-      , bench "all text" $ nf (toHumanized False) text
-      ]
+    [ benchTextToText "toUnderscore" toUnderscore
+    , benchTextToText "toDashed" toDashed
+    , benchTextToText "toCamelCased False" (toCamelCased False)
+    , benchTextToText "toHumanized False" (toHumanized False)
+    , benchTextToText "parameterize" parameterize
+    , bgroup "titleize"
+        [ bench "text-len-100" $ nf titleize (somewords t1)
+        , bench "text-len-10000" $ nf titleize (somewords t2)
+        , bench "text-len-1000000" $ nf titleize (somewords t3)
+        , bench "all text" $ nf titleize (somewords text)
+        ]
+    , bgroup "ordinal"
+        [ bench "1" $ nf ordinal 1
+        , bench "3" $ nf ordinal 13
+        , bench "13" $ nf ordinal 13
+        , bench "137" $ nf ordinal 137
+        ]
     ]
